be0ee595e582b164b0268434ae8a9d767a3cc1c0
rayhawkins <raymond.hawkins@mail.utoronto.ca>
2024-05-13 16:13:58 +0200

    Added SDT data_loader

--- src/data_processing.py
+++ src/data_processing.py
@@ -24,3 +24,74 @@ def compute_sdt(labels: np.ndarray, scale: int = 5):
 
     # be sure to return your solution as type 'float'
     return distance.astype(float)
+
+
+class SDTDataset(Dataset):
+    """A PyTorch dataset to load cell images and nuclei masks."""
+
+    def __init__(self, root_dir, transform=None, img_transform=None, return_mask=False):
+        self.root_dir = (
+            "/group/dl4miacourse/segmentation/" + root_dir
+        )  # the directory with all the training samples
+        self.samples = os.listdir(self.root_dir)  # list the samples
+        self.return_mask = return_mask
+        self.transform = (
+            transform  # transformations to apply to both inputs and targets
+        )
+        self.img_transform = img_transform  # transformations to apply to raw image only
+        #  transformations to apply just to inputs
+        inp_transforms = transforms.Compose(
+            [
+                transforms.Grayscale(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),  # 0.5 = mean and 0.5 = variance
+            ]
+        )
+
+        self.loaded_imgs = [None] * len(self.samples)
+        self.loaded_masks = [None] * len(self.samples)
+        for sample_ind in range(len(self.samples)):
+            img_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "image.tif"
+            )
+            image = Image.open(img_path)
+            image.load()
+            self.loaded_imgs[sample_ind] = inp_transforms(image)
+            mask_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "label.tif"
+            )
+            mask = Image.open(mask_path)
+            mask.load()
+            self.loaded_masks[sample_ind] = mask
+
+    # get the total number of samples
+    def __len__(self):
+        return len(self.samples)
+
+    # fetch the training sample given its index
+    def __getitem__(self, idx):
+        # We'll be using the Pillow library for reading files
+        # since many torchvision transforms operate on PIL images
+        image = self.loaded_imgs[idx]
+        mask = self.loaded_masks[idx]
+        if self.transform is not None:
+            # Note: using seeds to ensure the same random transform is applied to
+            # the image and mask
+            seed = torch.seed()
+            torch.manual_seed(seed)
+            image = self.transform(image)
+            torch.manual_seed(seed)
+            mask = self.transform(mask)
+        sdt = self.create_sdt_target(mask)
+        if self.img_transform is not None:
+            image = self.img_transform(image)
+        if self.return_mask is True:
+            return image, transforms.ToTensor()(mask), sdt
+        else:
+            return image, sdt
+
+    def create_sdt_target(self, mask):
+
+        sdt_target_array = compute_sdt(mask)
+        sdt_target = transforms.ToTensor()(sdt_target_array)
+        return sdt_target.float()
