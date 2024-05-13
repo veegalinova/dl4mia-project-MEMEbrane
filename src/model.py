085629a252497c12520f96b2d476cb1a0e033ae1
rayhawkins <raymond.hawkins@mail.utoronto.ca>
2024-05-13 16:08:54 +0200

    Added compute_std function

--- src/model.py
+++ src/model.py
@@ -4,6 +4,29 @@ import torch.nn as nn
 import numpy as np
 
 
+def compute_sdt(labels: np.ndarray, scale: int = 5):
+    """Function to compute a signed distance transform."""
+
+    # compute the distance transform inside and outside of the objects
+    labels = np.asarray(labels)
+    ids = np.unique(labels)
+    ids = ids[ids != 0]
+    inner = np.zeros(labels.shape, dtype=np.float32)
+
+    for id_ in ids:
+        inner += distance_transform_edt(labels == id_)
+    outer = distance_transform_edt(labels == 0)
+
+    # create the signed distance transform
+    distance = inner - outer
+
+    # scale the distances so that they are between -1 and 1 (hint: np.tanh)
+    distance = np.tanh(distance / scale)
+
+    # be sure to return your solution as type 'float'
+    return distance.astype(float)
+    
+
 class ConvBlock(torch.nn.Module):
     def __init__(
         self,
